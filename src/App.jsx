1d1febba1651d6e8e52583c44ca9f293b7922e2d
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2023-07-27 00:58:14 +0530

    update env & 1st api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect } from 'react'
+import { fetchDataFromAPI } from './utils/api'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
+  const apiTestting = async() =>{
+    const data = await fetchDataFromAPI('/movie/popular');
+    console.log(data);
+  }
+
+  useEffect(() => {
+    apiTestting();
+  }, []);
 
   return (
     <React.Fragment>
