629d30767f3649ca35c46c48e1fd0c056b6023f0
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2023-07-30 16:08:56 +0530

    details banner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import './styles.scss';
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import "./styles.scss";
+import { useParams } from "react-router-dom";
 
 const Details = () => {
+  const { mediaType, id } = useParams();
+  const { data, loading, error } = useFetch(`/${mediaType}/${id}/videos`);
+  const {
+    data: credits,
+    loading: creditdLoading,
+    error: creditsError,
+  } = useFetch(`/${mediaType}/${id}/credits`);
   return (
-    <div>Details</div>
-  )
-}
+    <div>
+      <DetailsBanner video={data?.results[0]} crew={credits?.crew} />
+    </div>
+  );
+};
 
 export default Details
