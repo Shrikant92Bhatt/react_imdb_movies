dfe2398ff301845f55998e3b1cbfafee681d8479
Shrikant Bhatt <shrikantbhatt92@gmail.com>
2023-08-01 01:11:58 +0530

    component added for search

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-import './styles.scss';
+import useFetch from "../../hooks/useFetch";
+import fetchDataFromApi from "../../utils/api";
+import contentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+import "./styles.scss";
 const SearchResult = () => {
-  return (
-    <div>SearchResult</div>
-  )
-}
+  return <div>SearchResult</div>;
+};
 
-export default SearchResult
+export default SearchResult;
